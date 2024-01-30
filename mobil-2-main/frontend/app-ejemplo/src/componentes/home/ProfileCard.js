39b8bc9c38dde3ee4dc8fed7fe7d396424ae3f88
javierc199 <javier131999@hotmail.es>
2024-01-30 17:55:13 -0500

    Se agrega el proyecto.

---
+++ mobil-2-main/frontend/app-ejemplo/src/componentes/home/ProfileCard.js
@@ -0,0 +1,88 @@
+import React from "react";
+import {View, Text, StyleSheet, Image, Linking,TouchableWithoutFeedback} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const twitter = <Icon name={'twitter'} size={30} color={'black'}/>
+const facebook = <Icon name={'facebook'} size={30} color={'black'}/>
+const instagram = <Icon name={'instagram'} size={30} color={'black'}/>
+const linkedin = <Icon name={'linkedin'} size={30} color={'black'}/>
+const tiktok = <Icon name={'sitemap'} size={30} color={'black'}/>
+const ProfileCard = () => {
+    const user = {
+        avatar: "https://aniyuki.com/wp-content/uploads/2021/06/aniyuki-anime-avatars-gif-discord-66.gif",
+        coverPhoto: "https://wallpapercave.com/wp/wp2730855.gif",
+        name: "Javier Catucuago"
+    }
+    return (
+        <View style={styles.container}>
+            <Image source={{uri: user.coverPhoto}} style={styles.coverPhoto}/>
+            <View style={styles.avatarContainer}>
+                <Image source={{uri: user.avatar}} style={styles.avatar}/>
+                <Text style={styles.name}>
+                    {user.name}
+                </Text>
+            </View>
+            <View style={styles.buttonContainer}>
+
+                <TouchableWithoutFeedback style={{color: 'blue'}} onPress={() => {
+                    Linking.openURL('https://facebook.com/')
+                }}>
+                    {facebook}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback style={{color: 'blue'}} onPress={() => {
+                    Linking.openURL('https://twitter.com/')
+                }}>
+                    {twitter}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback style={{color: 'blue'}} onPress={() => {
+                    Linking.openURL('https://instagram.com/')
+                }}>
+                    {instagram}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={()=>Linking.openURL('https://linkedin.com/')}>
+                    {linkedin}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={()=>Linking.openURL('https://tiktok.com/')}>
+                    {tiktok}
+                </TouchableWithoutFeedback>
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '50%',
+        alignItems: 'center' //flex y grid
+    },
+    coverPhoto: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover'
+
+    },
+    avatarContainer: {
+        alignItems: 'center',
+        marginTop: -75
+    },
+    avatar: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        borderWidth: 10,
+        borderColor: 'white'
+    },
+    name: {
+        marginTop: 15,
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+        width: '60%',
+        justifyContent: 'space-between'
+    }
+});
+export default ProfileCard
